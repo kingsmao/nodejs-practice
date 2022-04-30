2bb10ba11a0cb3475c85881996045572b121d241
kingsmao <>
2022-04-30 23:08:48 +0800

    up

--- day02/test_time.js
+++ day02/test_time.js
@@ -1,5 +1,6 @@
 const myDate = require('./dateFormat.js')
 const monent = require('moment')
+const myDate2 = require('../bender-tools/index.js')//index.js不写也行，自动找package.json中指定的main
 
 const dt = new Date()
 
@@ -13,3 +14,8 @@ console.log('------------------')
 
 const m = monent().format('YYYY-MM-DD HH:mm:ss')
 console.log(m)
+
+
+const newDt2 = myDate.dateFormat(dt)
+console.log('------- bender-tools -----------')
+console.log(newDt2)
